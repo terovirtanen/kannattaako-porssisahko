c6c17540ab61eacbca159eeb53450904c1c9e3fe
Tero Virtanen <tero.tapio.virtanen@gmail.com>
2025-02-06 22:09:38 +0200

    fix how margin is added to costs

--- src/pool_price.ts
+++ src/pool_price.ts
@@ -8,6 +8,8 @@ const energyFilePath = './data/Kulutusraportti_tunti_2024_2023.csv';
 // const filePath = './data/sahkon-hinta-010121-060225_short.csv';
 // const energyFilePath = './data/Kulutusraportti_tunti_2024_short.csv';
 
+const priceMargin = 0.5; // 0.5 cents
+
 const readPriceCSV = async (filePath: string) => {
     const fileStream = fs.createReadStream(filePath);
     const rl = readline.createInterface({
@@ -15,20 +17,20 @@ const readPriceCSV = async (filePath: string) => {
         crlfDelay: Infinity
     });
 
-    const data: { date: Date, price: number, energy: number, totalCost?: number }[] = [];
+    const data: { date: Date, price: number | undefined, energy: number | undefined, totalCost?: number }[] = [];
 
     for await (const line of rl) {
         const [date, price] = line.split(';');
         const formattedDate = parse(date, 'DD/MM/YYYY HH:mm:ss');
         formattedDate.setMinutes(0, 0, 0);
         const formattedPrice = parseFloat(price.replace(',', '.').replace('−', '-')); // Replace non-standard minus sign
-        data.push({ date: formattedDate, price: formattedPrice, energy: -1000 });
+        data.push({ date: formattedDate, price: formattedPrice, energy: undefined });
     }
 
     return data;
 };
 
-const readEnergyCSV = async (filePath: string, data: { date: Date, price: number, energy: number, totalCost?: number }[]) => {
+const readEnergyCSV = async (filePath: string, data: { date: Date, price: number | undefined, energy: number | undefined, totalCost?: number }[]) => {
     const fileStream = fs.createReadStream(filePath);
     const rl = readline.createInterface({
         input: fileStream,
@@ -44,25 +46,25 @@ const readEnergyCSV = async (filePath: string, data: { date: Date, price: number
         if (entry) {
             entry.energy = formattedEnergyValue;
         } else {
-            data.push({ date: formattedDate, price: -1000, energy: formattedEnergyValue });
+            data.push({ date: formattedDate, price: undefined, energy: formattedEnergyValue });
         }
     }
 
     data.forEach(entry => {
-        if (entry.price !== -1000 && entry.energy !== -1000) {
-            entry.totalCost = parseFloat(((entry.price * entry.energy) / 100).toFixed(2)); // Convert cents to euros
+        if (entry.price !== undefined && entry.energy !== undefined) {
+            entry.totalCost = parseFloat((((entry.price + priceMargin)* entry.energy) / 100).toFixed(2)); // Convert cents to euros
         }
     });
 };
 
-const generateReports = (data: { date: Date, price: number, energy: number, totalCost?: number }[]) => {
+const generateReports = (data: { date: Date, price: number | undefined, energy: number | undefined, totalCost?: number }[]) => {
     const monthlyReport: { [key: string]: { totalEnergy: number, totalCost: number } } = {};
     const yearlyReport: { [key: string]: { totalEnergy: number, totalCost: number } } = {};
     const winterMonths = [0, 1, 2, 3, 9, 10, 11]; // January, February, March, October, November, December
     const winterSummary: { [key: string]: { totalEnergy: number, totalCost: number } } = {};
 
     data.forEach(entry => {
-        if (entry.totalCost !== undefined && entry.price !== -1000 && entry.energy !== -1000) {
+        if (entry.totalCost !== undefined && entry.price !== undefined && entry.energy !== undefined) {
             const monthYear = entry.date.toLocaleString(undefined, { year: 'numeric', month: 'long' });
             const year = entry.date.getFullYear().toString();
             const month = entry.date.getMonth();
@@ -109,7 +111,7 @@ const processFiles = async () => {
     try {
         const data = await readPriceCSV(filePath);
         await readEnergyCSV(energyFilePath, data);
-        const filteredData = data.filter(entry => entry.price !== -1000 && entry.energy !== -1000);
+        const filteredData = data.filter(entry => entry.price !== undefined && entry.energy !== undefined);
         // filteredData.forEach(entry => {
         //     const shortDate = new Date(entry.date).toLocaleString(undefined, { dateStyle: 'short', timeStyle: 'short', hour12: false });
         //     console.log(`Date: ${shortDate}, Price: ${entry.price}, Energy: ${entry.energy}, Total Cost: €${entry.totalCost}`);
